722708af33be0440930bf7741e3436159f7e5b7b
Ayla Pearson <ayla@poissonconsulting.ca>
2023-07-13 16:02:19 -0700

    adding ui and server base

--- R/app-server.R
+++ R/app-server.R
@@ -1,4 +1,30 @@
 app_server <- function(input, output, session) {
 
+  data <- mod_upload_server(
+    "mod_upload_ui"
+  )
+
+  mod_map_server(
+    "mod_map_ui",
+    data
+  )
+
+  mod_plot_server(
+    "mod_plot_server",
+    data
+  )
+
+  mod_download_server(
+    "mod_download_server",
+    data
+  )
+
+  mod_help_server(
+    "mod_help_ui"
+  )
+
+  mod_about_server(
+    "mod_about_ui"
+  )
 
 }
--- R/app-ui.R
+++ R/app-ui.R
@@ -1,3 +1,78 @@
 app_ui <- function() {
-  tagList()
+  dashboardPage(
+    dark = NULL,
+    scrollToTop = TRUE,
+    controlbar = NULL,
+    footer = NULL,
+    header = dashboardHeader(
+      ... = div(
+        h3("Wood Bison Demographics"),
+        style = "vertical-align: baseline;"
+      )
+    ),
+    sidebar = dashboardSidebar(
+      skin = "light",
+      sidebarMenu(
+        id = "sidebarmenu",
+        menuItem(
+          "Upload Data",
+          tabName = "upload",
+          icon = icon("upload")
+        ),
+        menuItem(
+          "Map Locations",
+          tabName = "map",
+          icon = icon("map")
+        ),
+        menuItem(
+          "Plot Data",
+          tabName = "plot",
+          icon = icon("chart-gantt")
+        ),
+        menuItem(
+          "Download Data",
+          tabName = "download",
+          icon = icon("download")
+        ),
+        menuItem(
+          "Help",
+          tabName = "help",
+          icon = icon("question")
+        ),
+        menuItem(
+          "About",
+          tabName = "about",
+          icon = icon("info")
+        )
+      )
+    ),
+    body = dashboardBody(
+      tabItems(
+        tabItem(
+          tabName = "upload",
+          mod_upload_ui("mod_upload_ui")
+        ),
+        tabItem(
+          tabName = "map",
+          mod_map_ui("mod_map_ui")
+        ),
+        tabItem(
+          tabName = "plot",
+          mod_plot_ui("mod_plot_ui")
+        ),
+        tabItem(
+          tabName = "download",
+          mod_download_ui("mod_download_ui")
+        ),
+        tabItem(
+          tabName = "help",
+          mod_help_ui("mod_help_ui")
+        ),
+        tabItem(
+          tabName = "about",
+          mod_about_ui("mod_about_ui")
+        )
+      )
+    )
+  )
 }
